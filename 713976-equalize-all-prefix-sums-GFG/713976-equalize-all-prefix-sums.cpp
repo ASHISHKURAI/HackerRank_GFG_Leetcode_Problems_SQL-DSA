821586f806ab92dadd74d8e57c143a3354b42d50
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-19 23:11:43 +0530

    Added solution - LeetHub

---
+++ 713976-equalize-all-prefix-sums-GFG/713976-equalize-all-prefix-sums.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> optimalArray(vector<int> &arr) {
+        int n = arr.size();
+
+        vector<long long> pref(n);
+        pref[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + arr[i];
+        }
+
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++) {
+            int m = i / 2;
+
+            long long leftCost =
+                1LL * arr[m] * (m + 1) - pref[m];
+
+            long long rightCost =
+                (pref[i] - pref[m]) -
+                1LL * arr[m] * (i - m);
+
+            ans[i] = (int)(leftCost + rightCost);
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
