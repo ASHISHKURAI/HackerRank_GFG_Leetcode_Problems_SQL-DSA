38f8d9a44a39926906f78f9caf669f0fe4deeeb0
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-21 16:26:13 +0530

    Added solution - LeetHub

---
+++ 704867-choose-and-swap-GFG/704867-choose-and-swap.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    string chooseSwap(string &s) {
+        set<char>st;
+        for(char ch : s)
+            st.insert(ch);
+        char a = '*' , b = '*';
+        for(char ch : s){
+            if(st.find(ch) == st.end())
+                continue;
+            else if(ch == *st.begin())
+                st.erase(ch);
+            else{
+                a = *st.begin();
+                b = ch;
+                break;
+            }
+        } 
+        for(char &ch : s){
+            if(ch == a)
+                ch = b;
+            else if(ch == b)
+                ch = a;
+        }
+        return s;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
