ef757e584beaf5cea1c3d213df5a8c9e528b8f49
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-29 22:13:10 +0530

    Added solution - LeetHub

---
+++ 704035-max-dot-product-with-0-insertions-GFG/704035-max-dot-product-with-0-insertions.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    
+    int maxDotProduct(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size();
+        int m = b.size();
+        vector<int>dp(m+1,0);
+        
+        for(int i=n-1;i>=0;i--){
+            int next = 0;
+            for(int j=m-1;j>=0;j--){
+               
+               int temp2 = dp[j];
+               int temp = 0;
+               
+               if(n-i!=m-j) temp = dp[j];
+               dp[j] = max(temp,(a[i]*b[j]) + next); 
+               next = temp2;
+            }
+        }
+        
+        return dp[0];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
