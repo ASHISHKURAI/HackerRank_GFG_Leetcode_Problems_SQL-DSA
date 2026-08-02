8da8c6f3353e070c27b20dbe06b0fb6b689bff41
Ashish K Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-08-02 23:10:29 +0530

    Added solution - Time: O(n * m^2), Space: O(n * m)

---
+++ 705083-sequences-where-adjacent-divide-GFG/705083-sequences-where-adjacent-divide.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  
+    int solve(int ind, int prev, int n, int m, vector<vector<int>>&dp){
+        if(ind==n) return 1;
+        
+        if(dp[ind][prev]!=-1) return dp[ind][prev];
+        
+        int cnt=0;
+        for(int i=1; i<=m; i++){
+            if(((i%prev)==0) || ((prev%i)==0)){
+                cnt+=solve(ind+1, i, n, m, dp);
+            }
+        }
+        return dp[ind][prev]=cnt;
+    }
+  
+    int count(int n, int m) {
+        // code here
+        vector<vector<int>>dp(n, vector<int>(m+1, -1));
+        return solve(0, 1, n, m, dp);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
