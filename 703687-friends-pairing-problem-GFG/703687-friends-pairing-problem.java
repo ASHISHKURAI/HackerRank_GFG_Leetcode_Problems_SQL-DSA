8814287fb920147cc5339107b011de0acbb2090b
Ashish K Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-08-08 18:53:52 +0530

    Added solution - Time: O(2^n), Space: O(n)

---
+++ 703687-friends-pairing-problem-GFG/703687-friends-pairing-problem.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countFriendsPairings(int n) {
+        return solve(n);
+    }
+    private int solve(int n ){
+        if(n<=2)
+            return n;
+        int res = solve(n-1);
+        int pair=(n-1)*solve(n-2);
+        
+        return (res+pair);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
